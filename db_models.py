56e6911b5bb89fd672789a5f0310dee6a73af3c8
Lex <alexnosov2009@rambler.ru>
2023-08-14 14:30:41 +0300

    it worked

--- db_models.py
+++ db_models.py
@@ -1,11 +1,13 @@
 from peewee_aio import Manager, fields, AIOModel
 
-manager = Manager('aiosqlite:///:memory:')
+manager = Manager('aiosqlite:///db.sqlite')
+
 
 @manager.register
 class UserModel(AIOModel):
     user_id = fields.IntegerField()
     name = fields.CharField(max_length=50)
+
 
 @manager.register
 class RemainderModel(AIOModel):
--- remainder_db_handler.py
+++ remainder_db_handler.py
@@ -5,12 +5,12 @@ import asyncio
 
 from db_models import UserModel, RemainderModel, manager
 
+
 async def add_user(user_tg_id, first_name):
-    with manager.connection():
+    async with manager.connection():
         await UserModel.create_table()
         await RemainderModel.create_table()
 
         user = await UserModel.create(user_id=user_tg_id, name=first_name)
         assert user
         assert user.user_id
-        
